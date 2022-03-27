dd5def3a3325b0061c5946cc359b4d4af80e7ea4
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2022-03-28 05:16:42 +0900

    Initial Commit

---
+++ Shared/SwiftGenPluginExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGenPluginExampleApp.swift
+//  Shared
+//
+//  Created by shimastripe on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGenPluginExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
